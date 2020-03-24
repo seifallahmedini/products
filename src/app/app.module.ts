51e36aaf976fdbbf766ac330d7d03c87b32e6cc5
seifallahmedini <seifallahmedini@gmail.com>
2020-03-24 19:20:04 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { ProductsComponent } from "./products/products.component";
+import { AboutComponent } from "./about/about.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { routes } from "./app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    AboutComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, routes],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
